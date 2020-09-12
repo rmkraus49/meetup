1bad4e37cf04d94c7c15dfda015b96bf4b3ecea1
rmkraus49 <60583755+rmkraus49@users.noreply.github.com>
2020-09-12 19:46:33 -0400

    implemented atatus

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as atatus from 'atatus-spa';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,3 +16,4 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+atatus.config('fae8f7a64bbe434196c589a943e14646').install();
