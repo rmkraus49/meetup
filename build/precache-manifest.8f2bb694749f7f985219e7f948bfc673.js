79be383c9ac6dfc92257edd6f2c4bb5e09286dd0
rmkraus49 <60583755+rmkraus49@users.noreply.github.com>
2020-09-13 12:53:39 -0400

    added alert component

---
+++ build/precache-manifest.8f2bb694749f7f985219e7f948bfc673.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a70e0a78301967889ee9781be29910c2",
+    "url": "/meetup/index.html"
+  },
+  {
+    "revision": "d8d2af549c4ba446e4c9",
+    "url": "/meetup/static/css/main.bfb37e3b.chunk.css"
+  },
+  {
+    "revision": "d6ff715813f7776dbe50",
+    "url": "/meetup/static/js/2.d11ec866.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meetup/static/js/2.d11ec866.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8d2af549c4ba446e4c9",
+    "url": "/meetup/static/js/main.5eceb1d8.chunk.js"
+  },
+  {
+    "revision": "5b15bedc16c52474edcc",
+    "url": "/meetup/static/js/runtime-main.61f01ad6.js"
+  }
+]);
--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
 import { getSuggestions } from './api';
+import { InfoAlert } from './Alert';
 import './App.css';
 
 class CitySearch extends Component {
   state = {
     query: 'Munich',
     suggestions: [],
+    infoText: null,
   }
 
   handleInputChanged = (event) => {
     const value = event.target.value;
     this.setState({ query: value });
-    getSuggestions(value).then(suggestions => this.setState({ suggestions }));
+    getSuggestions(value).then(suggestions => {
+      this.setState({ suggestions });
+
+      if (value && suggestions.length === 0) {
+        this.setState({
+          infoText: 'We cannot find the city you are looking for. Please check the spelling or try another city.',
+        });
+      } else {
+        this.setState({
+          infoText: '',
+        });
+      }
+    });
   }
 
   handleItemClicked = (value, lat, lon) => {
@@ -22,6 +36,7 @@ class CitySearch extends Component {
   render() {
     return (
       <div className="CitySearch">
+        <InfoAlert text={this.state.infoText} />
         <input
           type="text"
           className="city"
