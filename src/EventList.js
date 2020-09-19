9f61d03cafc68e88dd9d4aa6818e4df943a1cf98
rmkraus49 <60583755+rmkraus49@users.noreply.github.com>
2020-09-19 15:53:03 -0400

    added offline functionality

--- src/EventList.js
+++ src/EventList.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import Event from './Event';
 import './App.css';
+import { InfoAlert } from './Alert';
 
 class EventList extends Component {
 
   render() {
     return (
       <ul className="EventList">
+        {!navigator.onLine &&
+          <InfoAlert text='You are currently offline. The list of events may not be up to date.' />
+        }
         {this.props.events.map((event) => (
           <li className="Event" key={event.id}>
             <Event event={event} />
