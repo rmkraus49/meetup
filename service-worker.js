122e0506988051c41e2a267f006bccbf57fbcc29
rmkraus49 <60583755+rmkraus49@users.noreply.github.com>
2020-09-20 17:36:24 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/meetup/precache-manifest.2e731b404528b015b858c778425773ae.js"
+  "/meetup/precache-manifest.973c220376b7d72b6ab39acb5943d459.js"
 );
 
 self.addEventListener('message', (event) => {
